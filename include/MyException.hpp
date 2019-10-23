c7e44d6deabfc78cd02db026b487d4b3fa6636bd
hing1 <hing123@126.com>
2019-10-23 09:18:15 +0800

    The exception of program

---
+++ include/MyException.hpp
@@ -0,0 +1,22 @@
+#ifndef MYEXCEPTION_H
+#define MYEXCEPTION_H
+
+
+class MeetingNoParticipator {};
+class NotRegistered_User {};
+class NotRegistered_Sponsor {};
+class NotRegistered_Participator {};
+class NameRepeat_user {};
+class NameRepeat_SponsorParticipator {};
+class NameRepeat_TWoParticipator {};
+class TitleRepeat {};
+class DateIllegal {};
+class SponsorBusy {};
+class ParticipatorBusy {};
+class PasswordError {};
+class ErrorFinishNone {};
+class NoneMeetingName {};
+class NoneParticipatorName {};
+class SponsorQuitMeeting {};
+
+#endif
